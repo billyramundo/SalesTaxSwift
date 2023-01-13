68691cddaa148f72d6dbc72fe930ec0d87253e1e
Billy Ramundo <william.ramundo@duke.edu>
2023-01-13 15:14:34 -0500

    Initial calc done

---
+++ SalesTaxSwift/SalesTaxSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesTaxSwiftApp.swift
+//  SalesTaxSwift
+//
+//  Created by Billy  Ramundo on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SalesTaxSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
